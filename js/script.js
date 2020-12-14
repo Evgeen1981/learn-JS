626d625a47f96ae476f998b77b96d7378ece3f14
Evgeen <evgeen@inbox.ru>
2020-12-14 19:46:51 +0300

    Practise2

--- js/script.js
+++ js/script.js
@@ -14,7 +14,22 @@ const personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов', ''),
         b = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[a] = b;
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count > 29) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
